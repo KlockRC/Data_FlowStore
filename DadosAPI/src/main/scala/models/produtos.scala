3d0c911762933d45d67b3cadab1b254151ade617
KlockRC <ruan082007@gmail.com>
2025-03-28 15:22:05 -0300

    fix:apiData

--- DadosAPI/src/main/scala/models/produtos.scala
+++ DadosAPI/src/main/scala/models/produtos.scala
@@ -6,13 +6,13 @@ import io.circe.generic.semiauto._
 case class Produto(
   product_id: String,
   product_category_name: String,
-  product_name_lenght: Int,
-  product_description_lenght: Int,
-  product_photos_qty: Int,
-  product_weight_g: Int,
-  product_length_cm: Int,
-  product_height_cm: Int,
-  product_width_cm: Int // Adicionado o novo campo
+  product_name_lenght: String,
+  product_description_lenght: String,
+  product_photos_qty: String,
+  product_weight_g: String,
+  product_length_cm: String,
+  product_height_cm: String,
+  product_width_cm: String 
 )
 
 object Produto {
