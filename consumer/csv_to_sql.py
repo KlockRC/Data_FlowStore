08ad6b951fd322ce355d27d1dfad06dd8db342a8
root <root@Cesar>
2025-04-17 23:19:36 -0300

    Feat: Add Consumer Scripts
    Add Script For CSV To SQL And SQL To Parquet on S3

---
+++ consumer/csv_to_sql.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+
+con = create_engine("postgresql://Locals:Locals@localhost:5432/Locals")
+chunk = pd.read_csv("local.csv", chunksize = 7000)
+
+while True:
+    try:
+        time1 = time()
+        df = next(chunk)
+        df.to_sql(name="Locals", con=con, if_exists="append", index=False)
+        time2 = time()
+        print("done %.3f" %(time2 - time1))
+    except StopIteration:
+        print("fim")
+        break
